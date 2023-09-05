ac23aa0e9ce7308947e0be347cc8782fb75d1538
Kacper Filipiuk <89936276+Varodann@users.noreply.github.com>
2023-09-05 17:46:42 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Form from './components/Pagees/Form';
-import FormWithOneRecipe from "./components/Pagees/FormWithOneRecipe";
+import Form from './components/pages/Form';
+import FormWithOneRecipe from "./components/pages/FormWithOneRecipe";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<Form />} />
+        <Route path="/home" element={<Form />} />
         <Route path="/recipe/:id" element={<FormWithOneRecipe />} />
     </Routes>
   );
